23305d7627b8be29c050e37ed1e68f4be50370ff
brenoximenes <breno.ximenes.ponte@altran.com>
2021-09-22 00:54:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import coockie from './cookie.png';
+import icon1 from './icon1.png';
+import icon2 from './icon2.png';
+import icon3 from './icon3.png';
+import ReactTooltip from 'react-tooltip';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count:0
+    }
+  }
+
+  incrementCookie = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+
+  increment10 = () => {
+    const interval = setInterval(() => {
+      this.setState({ count: this.state.count + 10});
+    }, 1000);
+    return () => clearInterval(interval);
+  }
+
+  increment20 = () => {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 20});
+    }, 2000);
+    return () => clearInterval(setInterval);
+  }
+
+  increment30 = () => {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 20});
+    }, 4000);
+    return () => clearInterval(setInterval);
+  }
+
+  render() {
+    return (
+      <div className="overlay">
+      <div className="App">
+        <h1 className='title'>Click the Cookie</h1>
+    
+        <div className="cookie-wrapper">
+        <img src={coockie} onClick={this.incrementCookie}
+        className="cookie" alt="Cookie" />
+        </div>
+
+        <div className="cookie-counter">
+        <h1>{this.state.count} Cookies</h1>
+        </div>
+        
+        <div className="buttons">
+    
+          <span ref={ref => this.fooRef = ref} 
+          data-tip='You have to Bake 50 Cookies to unlock'
+          data-background-color='#B77234'>
+          <button  disabled={this.state.count < 50} 
+            className="btn" onTouchStart={() => { ReactTooltip.show(this.fooRef) }} 
+            onClick={ this.increment10}>
+            <img className="icon" src={icon1} alt="iconcookie" />
+            <p className="btn-text"> +10 IN 1 SEC </p>
+          </button>      
+          <ReactTooltip />
+          </span>
+            
+          <span ref={ref => this.fooRef = ref} 
+          data-tip='You have to Bake 600 Cookies to unlock'
+          data-background-color='#B77234'>
+          <button  disabled={this.state.count < 600} 
+            className="btn" onTouchStart={() => { ReactTooltip.show(this.fooRef) }} 
+            onClick={ this.increment20}>
+            <img className="icon" src={icon2} alt="iconcookie" />
+            <p className="btn-text"> +20 IN 2 SEC </p>
+          </button>      
+          <ReactTooltip />
+          </span>
+
+          <span ref={ref => this.fooRef = ref} 
+          data-tip='You have to Bake 5000 Cookies to unlock'
+          data-background-color='#B77234'>
+          <button  disabled={this.state.count < 5000} 
+            className="btn" onTouchStart={() => { ReactTooltip.show(this.fooRef) }} 
+            onClick={ this.increment10}>
+            <img className="icon" src={icon3} alt="iconcookie" />
+            <p className="btn-text"> +30 IN 4 SEC </p>
+          </button>      
+          <ReactTooltip />
+          </span>
+        
+        </div>  
+      </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
